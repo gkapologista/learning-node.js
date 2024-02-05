7ff09cb228a4843687e0b0ca154a90e61878f26b
gkapologista <gkapologista0800@gmail.com>
2024-02-05 14:09:55 +0800

    1:18:59

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 //@desc Register a user
@@ -20,6 +21,18 @@ const registerUser = asyncHandler(async (req, res) => {
     //Hash password
     const hashedPassword = await bcrypt.hash(password, 10);
     console.log("Hashed Password: ", hashedPassword);
+    const user = await User.create({
+        username,
+        email,
+        password: hashedPassword,
+    });
+    console.log(`User created ${user}`);
+    if(user) {
+        res.status(201).json({_id: user.id, email: user.email });
+    } else {
+        res.status(400);
+        throw new Error("User data is not valid");
+    }
     res.json({ message: "Register User" });
 });
 
@@ -27,7 +40,28 @@ const registerUser = asyncHandler(async (req, res) => {
 //@router POST /api/users/login
 //@access public
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({ message: "Login User" });
+    const { email, password } = req.body;
+    if (!email || !password ) {
+        res.status(400);
+        throw new Error("Fields are required");
+    }
+    const user = await User.findOne({ email });
+    //compare password with hashed password
+    if(user && (await bcrypt.compare(password, user.password))) {
+        const accessToken = jwt.sign({
+            user: {
+                username: user.username,
+                email: user.email,
+                id: user.id,
+            },
+        }, process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1m" }
+        );
+        res.status(200).json({ accessToken });
+    } else {
+        res.status(401);
+        throw new Error("Email or password is incorrect");
+    }
 });
 
 //@desc Current user
